fe22a91598892000a1a6a628c77877ba22adda70
yuta takahashi <m1205123ing@gmail.com>
2013-10-27 21:41:12 +0900

    initialize

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TryGit::Application.config.secret_key_base = '7c944394edf52aa3b9a1c7cebc3248a29115f0ade9869ef087765ef915b40efbc36d79d6fd3f16ba1c7106d0197d7b0d588db2b190b5bdfe8256409c3a094a5e'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+TryGit::Application.config.session_store :cookie_store, key: '_try_git_session'
